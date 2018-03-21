ce15c63c8858e07d602d2f2c1897a86edf7a11a2
Petr Tichy <petr.tichy@carsales.com.au>
2018-03-21 14:24:34 +1100

    user service

--- src/app/stores/viewStore.ts
+++ src/app/stores/viewStore.ts
@@ -5,69 +5,66 @@ import { User } from '../user';
 
 @Injectable()
 export class ViewStore {
-    @observable text: string = 'mobx store connected to angular component';
-    @observable users: User[] = [];
-    @observable selectedUser: User = null;
-    @observable fetchingData;
-    usersUrl = 'https://jsonplaceholder.typicode.com/users';
+  @observable text: string = 'mobx store connected to angular component';
+  @observable users: User[] = [];
+  @observable selectedUser: User = null;
+  @observable fetchingData;
+  usersUrl = 'https://jsonplaceholder.typicode.com/users';
 
-    constructor(){}
+  constructor() {}
 
-    @computed get userCount(){
-        return this.users.length;
-    }
+  @computed
+  get userCount() {
+    return this.users.length;
+  }
 
-    @action('Fetch users')
-    fetchUsers = async () => {
-        this.fetchingData = true;
-        const [users] = await Promise.all([
-            this.fetchFromApi(this.usersUrl)
-        ]);
-        runInAction("Update State after fetching Users from API", () => {
-            this.setUsers(users);
-            this.fetchingData = false;
-        });
+  @action('Fetch users')
+  fetchUsers = async () => {
+    this.fetchingData = true;
+    const [users] = await Promise.all([this.fetchFromApi(this.usersUrl)]);
+    runInAction('Update State after fetching Users from API', () => {
+      this.setUsers(users);
+      this.fetchingData = false;
+    });
+  };
 
-    }
+  @action('Fetch user')
+  fetchUser = async id => {
+    this.fetchingData = true;
+    const [user] = await Promise.all([
+      this.fetchFromApi(`${this.usersUrl}/${id}`)
+    ]);
+    runInAction('Update State after fetching User from API', () => {
+      this.setUser(user);
+      this.fetchingData = false;
+    });
+  };
 
-    @action('Fetch user')
-    fetchUser = async (id) => {
-        this.fetchingData = true;
-        const [user] = await Promise.all([
-            this.fetchFromApi(`${this.usersUrl}/${id}`)
-        ]);
-        runInAction("Update State after fetching User from API", () => {
-            this.setUser(user);
-            this.fetchingData = false;
-        });
+  async fetchFromApi(endpoint) {
+    const url = endpoint;
+    const response = await fetch(url);
+    return await response.json();
+  }
 
-    }
+  @action('Set users')
+  setUsers(users) {
+    this.users = users;
+  }
 
-    async fetchFromApi(endpoint) {
-        const url = endpoint;
-        const response = await fetch(url);
-        return await response.json();
-    }    
+  @action('Set user')
+  setUser(user) {
+    this.users = [user];
+    this.selectedUser = user;
+  }
 
-    @action('Set users')
-    setUsers(users) {
-        this.users = users;
-    }
+  @action('Unselect user')
+  unselectUser() {
+    this.selectedUser = null;
+  }
 
-    @action('Set user')
-    setUser(user) {
-        this.users = [user];
-        this.selectedUser = user;
-    }
-
-    @action('Unselect user')
-    unselectUser() {
-        this.selectedUser = null;
-    }
-
-    @action('Remove user')
-    removeUser(user) {
-        console.log(user.name);
-        this.users = this.users.filter(u => u !== user);
-    }
+  @action('Remove user')
+  removeUser(user) {
+    console.log(user.name);
+    this.users = this.users.filter(u => u !== user);
+  }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,26 +1,24 @@
-import { Injectable } from '@angular/core';
-import { User } from './user';
-import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {User} from './user';
+import {Observable} from 'rxjs/Observable';
+import {of} from 'rxjs/observable/of';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable()
 export class UserService {
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http : HttpClient) {}
 
-  private usersUrl = 'https://jsonplaceholder.typicode.com/users';  // URL to web api
+  private usersUrl = 'https://jsonplaceholder.typicode.com/users'; // URL to web api
 
-  getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.usersUrl);
+  getUsers() : Observable < User[] > {
+    return this.http.get < User[] > (this.usersUrl);
   }
 
   /** GET user by id. Will 404 if id not found */
-  getUser(id: number): Observable<User> {
+  getUser(id : number) : Observable < User > {
     const url = `${this.usersUrl}/${id}`;
-    return this.http.get<User>(url)
+    return this.http.get < User > (url)
   }
 
 }
